058693a4b3d3a7b6d8d50faccdccc09b0d8e3191
pg2429 <pg2429@nyu.edu>
2022-02-26 19:20:07 -0800

    fixed

--- solution.py
+++ solution.py
@@ -19,36 +19,43 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
+    # print(recv1)
 
     # Send MAIL FROM command and handle server response.
     mailFrom = 'MAIL FROM:<pg2429@nyu.edu\r\n'
     clientSocket.send(mailFrom.encode())
     recv2 = clientSocket.recv(1024).decode()
+    # print(recv2)
 
     # Send RCPT TO command and handle server response.
     rcptTo = 'RCPT TO:<pg2429@nyu.edu>\r\n'
     clientSocket.send(rcptTo.encode())
     recv3 = clientSocket.recv(1024).decode()
+    # print(recv3)
 
     # Send DATA command and handle server response.
     data = 'DATA\r\n'
     clientSocket.send(data.encode())
     recv4 = clientSocket.recv(1024).decode()
+    # print(recv4)
 
     # Send message data.
-    clientSocket.send(msg.encode())
+    clientSocket.send((msg+endmsg).encode())
     recv5 = clientSocket.recv(1024).decode()
+    # print(recv5)
 
 
     # Message ends with a single period, send message end and handle server response.
-    clientSocket.send(endmsg.encode())
-    recv6 = clientSocket.recv(1024).decode()
+    # clientSocket.send(endmsg.encode())
+    # recv6 = clientSocket.recv(1024).decode()
+    # print(recv6)
 
     # Send QUIT command and handle server response.
     quit = 'QUIT\r\n'
     clientSocket.send(quit.encode())
     recv7 = clientSocket.recv(1024).decode()
-    
+    # print(recv7)
+
     clientSocket.close()
 
 if __name__ == '__main__':
